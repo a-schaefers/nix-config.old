474df7ece94b51dcbc542e2c523d7cef056ea2cd
Adam Schaefers <paxchristi888@gmail.com>
2019-05-03 20:49:08 -0700

    modify scratch page

--- dotfiles/adam.el
+++ dotfiles/adam.el
@@ -1,8 +1,7 @@
-;; Adam's HOME  ¯\_(ツ)_/¯
+;; My persistent scratch  ¯\_(ツ)_/¯
 (find-file "~/org/bookmarks.org")
 (find-file "~/org/calendar.org")
 (find-file "~/org/clock.org")
 (find-file "~/org/todo.org")
-(find-file "~/Private/contacts.el")
 
 (+ 3 2) ;; calculator

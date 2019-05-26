08e338f8d90bc736622af384078c3e86c381a00c
Adam Schaefers <paxchristi888@gmail.com>
2019-05-26 02:12:14 -0700

    dotfiles

--- dotfiles/bin/dotfiles-install.sh
+++ dotfiles/bin/dotfiles-install.sh
@@ -1,20 +1,3 @@
-cat << EOF > "$HOME/.xserverrc"
-#!/bin/sh
-exec /usr/bin/Xorg -nolisten tcp -nolisten local "\$@" vt$XDG_VTNR
-EOF
-
-[ ! -d "$HOME/mpv" ] && mkdir -p "$HOME/.config/mpv"
-cat << EOF > "$HOME/.config/mpv/mpv.conf"
-profile=gpu-hq
-scale=ewa_lanczossharp
-cscale=ewa_lanczossharp
-video-sync=display-resample
-interpolation
-tscale=oversample
-
-x11-bypass-compositor=yes
-EOF
-
 dotfiles="/nix-config/dotfiles"
 [ ! -d "$HOME/.emacs.d/" ] &&  mkdir "$HOME/.emacs.d"
 ln -sf "$dotfiles/.emacs.d/"* "$HOME/.emacs.d"
--- hosts/latitude/postinstall.sh
+++ hosts/latitude/postinstall.sh
@@ -1,4 +1,5 @@
 # setup dotfiles
 cat << EOF | nixos-enter
+chown -R adam:users /nix-config
 sudo -u adam bash /nix-config/dotfiles/bin/dotfiles-install.sh
 EOF

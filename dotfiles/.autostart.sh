acf26640982c78c6136e1bdfa3adc4eaa295f988
Adam Schaefers <paxchristi888@gmail.com>
2019-06-06 05:01:14 -0700

    wip

--- dotfiles/.autostart.sh
+++ dotfiles/.autostart.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+simple-power-manager -d
+
 # shut off screen after 5 minutes idle time
 xset +dpms
 xset dpms 300
 
-# make sure only one set of loops are looping
+# loop control
 if pidof -o %PPID -x ".autostart.sh"; then exit; fi
 
 # slock screen after 5-6 minutes of idle time
 while true; do
     if [ "$(xprintidle-ng)" -gt 300000 ]; then
         pgrep slock || slock
-        sleep 60
     fi
+    sleep 60
 done &

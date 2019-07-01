e9597aec60edf89841e153c94fa0e1e6576455c9
Adam Schaefers <paxchristi888@gmail.com>
2019-06-30 23:31:01 -0700

    disable autoupgrade

--- hosts/latitude/default.nix
+++ hosts/latitude/default.nix
@@ -18,7 +18,7 @@ modules.desktop.enable = true;
 # modules.services.libvirtd.enable = true;
 
 # Experimental
-system.autoUpgrade.enable = true;
+# system.autoUpgrade.enable = true;
 boot.zfs.enableUnstable = true;
 boot.zfs.requestEncryptionCredentials = true;
 }

8bcb84fa86eb1d3a9e71dfbd34cf66cd3a797eff
Adam Schaefers <paxchristi888@gmail.com>
2019-07-01 22:44:43 -0700

    remove cruft

--- modules/desktop/exwm.nix
+++ modules/desktop/exwm.nix
@@ -4,49 +4,6 @@ let
 myEmacs = (pkgs.emacs.override {withGTK3=false; withGTK2=false; withX=true;});
 emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
 
-stupid-power-manager = pkgs.writeScriptBin "stupid-power-manager" ''
-die() {
-    [ $# -gt 0 ] && printf -- "%s\n" "(SPM) $*"
-    exit 1
-}
-
-[ "$(pidof -o %PPID -x "''${0##*/}")" ] && die "Stupid Power Manager is already running"
-
-batt_thresholds="99 80 40 20 10 5"
-
-[ ! -d "$HOME/.config/stupid-power-manager/state" ] && \
-    mkdir -p "$HOME/.config/stupid-power-manager/state"
-[ ! -f "$HOME/.config/stupid-power-manager/config" ] && \
-    die "Please create $HOME/.config/stupid-power-manager/config and try again."
-
-rm -f "$HOME/.config/stupid-power-manager/state"/* > /dev/null 2>&1 #shh
-
-while true; do
-    . "$HOME/.config/stupid-power-manager/config"
-    batt_status="$(cat /sys/class/power_supply/BAT0/status)"
-    batt_percent="$(cat /sys/class/power_supply/BAT0/capacity)"
-    if [ "$batt_status" = "Discharging" ]; then
-        echo "$batt_thresholds" | tr ' ' '\n' | while read -r state; do
-            if [ "$batt_percent" -eq "$state" ] || [ "$batt_percent" -lt "$state" ]; then
-                if [ ! -f "$HOME/.config/stupid-power-manager/state/$state" ]; then
-                    rm -f "$HOME/.config/stupid-power-manager/state/100"
-                    touch "$HOME/.config/stupid-power-manager/state/$state"
-                    user_custom_low_battery_hook
-                fi
-            fi
-        done
-    fi
-    if [ "$batt_status" = "Charging" ] || [ "$batt_status" = "Full" ]; then
-        if [ ! -f "$HOME/.config/stupid-power-manager/state/100" ]; then
-            rm -f "$HOME/.config/stupid-power-manager/state"/* > /dev/null 2>&1 #shh
-            touch "$HOME/.config/stupid-power-manager/state/100"
-            user_custom_battery_normal_hook
-        fi
-    fi
-    sleep 2
-done
-'';
-
 my-dots = pkgs.writeScriptBin "my-dotfiles" ''
 mkdir -p "$HOME"/{Downloads,Pictures,Documents}
 
@@ -135,57 +92,6 @@ cat << EOF > "$HOME/.icons/default/index.theme"
 [icon theme]
 Inherits=Adwaita
 EOF
-
-mkdir -p "$HOME/.config/stupid-power-manager/state"
-cat << EOF > "$HOME/.config/stupid-power-manager/config"
-# example desktop notification command
-notifycmd() { emacsclient -e '(battery)' }
-
-# example backlight commands
-# backlightcmd="xbacklight -set"
-# backlightcmd="light -S"
-backlightcmd="brightnessctl s" ; optPercent="%"
-
-user_custom_low_battery_hook() {
-    if [ "\$batt_percent" -lt 100 ] && [ "\$batt_percent" -gt 80 ]; then
-        # 80-99% batt_percentery
-        notifycmd
-        \$backlightcmd 100\$optPercent
-
-     elif [ "\$batt_percent" -lt 81 ] && [ "\$batt_percent" -gt 40 ]; then
-        # 40-80% battery
-        notifycmd
-        \$backlightcmd 80\$optPercent
-
-     elif [ "\$batt_percent" -lt 41 ] && [ "\$batt_percent" -gt 20 ]; then
-        # 20-40% battery
-        notifycmd
-        \$backlightcmd 40\$optPercent
-
-     elif [ "\$batt_percent" -lt 21 ] && [ "\$batt_percent" -gt 10 ]; then
-        # 10-20% battery
-        notifycmd
-        \$backlightcmd 20\$optPercent
-
-     elif [ "\$batt_percent" -lt 11 ] && [ "\$batt_percent" -gt 5 ]; then
-        # 5-10% battery
-        notifycmd
-        backlight_low
-
-     elif [ "\$batt_percent" -lt 6 ]; then
-        # 5% battery or less
-        notifycmd
-        \$backlightcmd 5\$optPercent
-        systemctl suspend
-    fi
-}
-
-user_custom_battery_normal_hook() {
-    # battery Charging / Full
-    notifycmd
-    \$backlightcmd 100\$optPercent
-}
-EOF
 '';
 in
 {
@@ -202,7 +108,6 @@ xset +dpms
 xset s 1800
 xset dpms 0 0 1860
 my-dotfiles
-stupid-power-manager &
 xrdb -merge ~/.Xresources
 xsetroot -cursor_name left_ptr
 emacs &
@@ -220,7 +125,7 @@ _JAVA_AWT_WM_NONREPARENTING = "1";
 };
 
 environment.systemPackages = with pkgs; [
-my-dots stupid-power-manager
+my-dots
 
 (emacsWithPackages (epkgs: (with epkgs.melpaPackages; [
 epkgs.xelb

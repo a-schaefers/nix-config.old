d7a78fb74d66c13cda4880e6c156f3ca810fbd65
Adam Schaefers <paxchristi888@gmail.com>
2019-07-15 22:57:56 -0700

    steam

---
+++ modules/desktop/gaming.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, callPackage, ... }:
+with lib;
+{
+options.modules.desktop.gaming.enable = mkEnableOption "modules.desktop.gaming";
+config = mkIf config.modules.desktop.gaming.enable {
+
+hardware.opengl.driSupport32Bit = true;
+hardware.pulseaudio.support32Bit = true;
+nixpkgs.config.allowUnfree = true;
+
+environment.systemPackages = with pkgs; [
+steam
+playonlinux
+];
+
+};
+}

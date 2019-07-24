d7b10e91b27031ccbd87a3b7756e750eef8427f5
Adam Schaefers <paxchristi888@gmail.com>
2019-07-24 12:42:22 -0700

    wip

--- hosts/Z620/quirks.nix
+++ hosts/Z620/quirks.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 # misc machine-unique settings that don't precisely fit into my various modules.
 {
-# amdgpu.audio parameter needed by amd rx460
-boot.kernelParams = [ "amdgpu.audio=0" ];
+
+boot.kernelParams = [
+"amdgpu.audio=0" # amdgpu.audio parameter needed by amd rx460
+"memmap=64K$0" "memory_corruption_check=0" # https://bbs.archlinux.org/viewtopic.php?id=189483
+];
 }

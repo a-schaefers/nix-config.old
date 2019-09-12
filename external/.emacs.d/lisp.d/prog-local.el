1e37caddcfa392b95e015bd4f22dceb754f6c661
Adam Schaefers <paxchristi888@gmail.com>
2019-09-12 16:55:51 -0700

    work in progress

--- external/.emacs.d/lisp.d/prog-local.el
+++ external/.emacs.d/lisp.d/prog-local.el
@@ -57,14 +57,6 @@
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'elisp-slime-nav-mode))
 
-;; (require 'paredit)
-;; (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-;; ;; enable in the *scratch* buffer
-;; (add-hook 'lisp-interaction-mode-hook 'paredit-mode)
-;; (add-hook 'ielm-mode-hook 'paredit-mode)
-;; (add-hook 'lisp-mode-hook 'paredit-mode)
-;; (add-hook 'eval-expression-minibuffer-setup-hook 'paredit-mode)
-
 (require 'lispy)
 (defun conditionally-enable-lispy ()
   (when (eq this-command 'eval-expression)
@@ -79,7 +71,7 @@
                      cider))
 
 (require 'clojure-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook (lambda () (lispy-mode 1)))
 (add-hook 'clojure-mode-hook 'subword-mode)
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 
@@ -87,15 +79,8 @@
 (setq nrepl-log-messages t)
 (add-hook 'cider-mode-hook 'eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'eldoc-mode)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook (lambda () (lispy-mode 1)))
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-
-;;;; HASKELL
-
-(require 'haskell-mode)
-(add-hook 'haskell-mode-hook 'subword-mode)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'haskell-mode-hook 'haskell-doc-mode)
 
 ;;;; NIX
 

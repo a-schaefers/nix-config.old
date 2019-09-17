b726bae3e15671f5f3e6f9b4fe032d67cb139eaa
Adam Schaefers <paxchristi888@gmail.com>
2019-09-16 20:00:55 -0700

    wip

--- external/.emacs.d/lisp.d/prog-local.el
+++ external/.emacs.d/lisp.d/prog-local.el
@@ -109,6 +109,7 @@
 (require 'js2-mode)
 (if (>= emacs-major-version '27)
     (add-hook 'js-mode-hook 'js2-minor-mode)
-  (add-to-list 'auto-mode-alist '("\\.js\\'"  . js2-mode)))
-(add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.js\\'"  . js2-mode))
+    (add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
+    (add-to-list 'interpreter-mode-alist '("node" . js2-mode))))
